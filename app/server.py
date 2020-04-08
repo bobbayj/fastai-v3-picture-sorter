6ab0da0b7535fbcfdb2154e0b9d140fe63c22b16
Bob <35100300+bobbayj@users.noreply.github.com>
2020-04-08 22:30:31 +1000

    fix export_file_url link to direct download

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/842y999wwghj7vt/export.pkl?dl=0'
+export_file_url = 'https://www.dropbox.com/s/842y999wwghj7vt/export.pkl?dl=1'
 export_file_name = 'export.pkl'
 
 classes = ['instagram', 'memes', 'screenshots']
